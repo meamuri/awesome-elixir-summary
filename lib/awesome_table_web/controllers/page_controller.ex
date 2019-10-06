7334951a414494c52ac651ba748bb3324fb2f39b
meamruri <meammf@yandex.ru>
2019-10-06 19:36:03 +0300

    fix controller, add default query param

--- lib/awesome_table_web/controllers/page_controller.ex
+++ lib/awesome_table_web/controllers/page_controller.ex
@@ -1,8 +1,24 @@
 defmodule AwesomeTableWeb.PageController do
   use AwesomeTableWeb, :controller
 
-  def index(conn, %{"min_stars" => min_stars} \\ %{}) do
+  def index(conn, parameters) do
     libs = AwesomeTable.Libraries.list_libraries
-    render(conn, "index.html", records: libs)
+    min_stars = with min_stars_parameter <- parameters["min_stars"] do
+      cond do
+        min_stars_parameter != nil -> min_stars_parameter
+        true -> "0"
+      end
+    end
+    stars_lower_boundary = Integer.parse(min_stars)
+    lower_boundary = case stars_lower_boundary do
+      {val, _} -> val
+      :error -> 0
+    end
+
+    render(conn, "index.html", records: Enum.filter(libs, fn r -> r.stars >= lower_boundary end))
   end
+
+#  def index(conn) do
+#    index(conn, %{"min_stars" => 0})
+#  end
 end
