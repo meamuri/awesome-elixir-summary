f2c7195054afb919229b8f3784bb9f6e907268aa
meamruri <meammf@yandex.ru>
2019-10-08 21:00:51 +0300

    add base sorting

--- lib/awesome_table_web/controllers/page_controller.ex
+++ lib/awesome_table_web/controllers/page_controller.ex
@@ -5,6 +5,7 @@ defmodule AwesomeTableWeb.PageController do
     min_stars = calc_lower_boundary(parameters)
 
     libs = get_libraries(AwesomeTable.Model.latest_request(), min_stars)
+      |> Enum.sort_by(fn e -> e.title end)
     render(conn, "index.html", records: libs)
   end
 
